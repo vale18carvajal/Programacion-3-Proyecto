b3ba0af89bc3926dbea4ee106271813272028ba9
Valeria Carvajal <valeriacarvajal1@outlook.com>
2024-03-07 15:12:32 -0600

    Cambio de color al boton

--- index.php
+++ index.php
@@ -1,43 +1,19 @@
 <?php include "shared/header.php" ?>
-    <main>
-      <div class="contenedor-titulo">
-        <!--<video src="https://i.imgur.com/J1hUeTH.mp4" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"></video>-->
-      </div>
-
-
-
-
-      Hello aquí va el contenido:P
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
-
-      <br>
+<main>
+  <div class="contenedor-titulo">
+    <!--<video src="https://i.imgur.com/J1hUeTH.mp4" autoplay="true" muted="true" loop="true" poster="https://carontestudio.com/img/contacto.jpg"></video>-->
+  </div>
+  <div class="row">
+    <h2>Búsqueda</h2>
+    <div class="col-md-6 col-lg-6">
+      <label for="">Tipo de propiedad</label>
+      <select name="" id="">
+        <option value="casa">Casa</option>
+        <option value="apartamento">Apartamento</option>
+        <option value="propiedad">Propidad</option>
+      </select>
+    </div>
+  </div>
 
 
 
@@ -45,6 +21,5 @@
 
 
 
-
-    </main>
-    <?php include "shared/footer.php" ?>
+</main>
+<?php include "shared/footer.php" ?>
