cc49eb8b6df90f72264159cf30072b7f18e17ef3
Valeria Carvajal <valeriacarvajal1@outlook.com>
2024-03-07 15:45:11 -0600

    Arreglo de formulario de contacto

--- contactenos.php
+++ contactenos.php
@@ -1,16 +1,16 @@
 <?php include "shared/header.php" ?>
 
-    <main class="row justify-content-center">
-        <div class="col-md-6 col-lg-6">
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="nombre" name="nombre">
-                <label for="nombre">Nombre</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="email" class="form-control" id="email" name="email">
-                <label for="email">Email</label>
-            </div>
-            <div class="mb-3">
+<main class="justify-content-center container">
+    <div class="row">
+        <div class="col-md-6 col-lg-6 form-floating mb-3">
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="name@example.com">
+            <label for="nombre">   Nombre</label>
+        </div>
+        <div class="col-md-6 col-lg-6 form-floating mb-3">
+            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            <label for="email">   Email</label>
+        </div>
+            <div class="col-md-6 col-lg-6 mb-3">
                 <label for="asunto">Asunto</label>
                 <select id="asunto" name="asunto" class="form-select">
                     <option value="1">Preguntas</option>
@@ -19,8 +19,8 @@
                 </select>
             </div>
         </div>
-        <div class="col-md-12 col-lg-12">
-        <div class=" mb-3">
+        <div class="row">
+            <div class="col-md-12 col-lg-12 mb-3">
                 <label for="mensaje">Descripción</label>
                 <textarea class="form-control" id="mensaje" name="mensaje" cols="30" rows="10"></textarea>
             </div>
@@ -28,9 +28,8 @@
                 <button class="btn btn-azul">Guardar</button>
             </div>
         </div>
-        
-    </main>
-    
+</main>
 
 
-    <?php include "shared/footer.php" ?>
+
+<?php include "shared/footer.php" ?>
