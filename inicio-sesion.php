dc067ea21aa2dbaae61f315a91ba9c89487546ae
Valeria Carvajal <valeriacarvajal1@outlook.com>
2024-03-28 15:13:45 -0600

    Actualización de la ventana de inicio sesión (arreglos formulario crear usuario)

--- inicio-sesion.php
+++ inicio-sesion.php
@@ -8,10 +8,10 @@
         <br>
         <div class="col-md-12 col-lg-12">
             <label for="usuario" class="form-label">Nombre de usuario</label>
-            <input id="usuario" class="form-control" type="text" placeholder="Nombre_Usuario">
+            <input id="usuario" class="form-control" type="text" placeholder="Nombre_Usuario" required>
             <br>
             <label for="clave" class="form-label">Contraseña</label>
-            <input id="clave" class="form-control" type="password" placeholder="••••••••••••••••">
+            <input id="clave" class="form-control" type="password" placeholder="••••••••••••••••" required>
             <div id="recuperar" class="form-text"> <a href="">He olvidado mi contraseña</a></div>
             <br>
             <button type="submit" class="btn btn-success">Ingresar</button>
@@ -19,51 +19,56 @@
         </div>
     </div>
 
-    <!-- d-flex -->
-    <div class="row crear-usuario">
+    <form class="row crear-usuario">
         <h2>Formulario para crear tu usuario</h2>
-        <br>
-        <div class="col-md-12 col-lg-6">
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="cedula" class="form-label">Cédula</label>
-            <input id="cedula" class="form-control" type="text" placeholder="112233445">
-            <br>
+            <input name="cedula" id="cedula" class="form-control" type="number" placeholder="112233445" min="100000000" max="999999999" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="nombre" class="form-label">Nombre</label>
-            <input id="nombre" class="form-control" type="text" placeholder="Nombre">
-            <br>
+            <input name="nombre" id="nombre" class="form-control" type="text" placeholder="Nombre" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="apellido1" class="form-label">Primer Apellido</label>
-            <input id="apellido1" class="form-control" type="text" placeholder="Apellido1">
-            <br>
+            <input name="apellido1" id="apellido1" class="form-control" type="text" placeholder="Apellido1" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="apellido2" class="form-label">Segundo Apellido</label>
-            <input id="apellido2" class="form-control" type="text" placeholder="Apellido2">
-            <br>
+            <input name="apellido2" id="apellido2" class="form-control" type="text" placeholder="Apellido2" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="nacimiento" class="form-label">Fecha de Nacimiento</label>
-            <input id="nacimiento" class="form-control" type="date">
-            <br>
+            <input name="nacimiento" id="nacimiento" class="form-control" type="date" required>
         </div>
-        <div class="col-md-12 col-lg-6">
+        <div class="col-md-12 col-lg-6 mt-3">
             <label for="correo" class="form-label">Correo Electrónico</label>
-            <input id="correo" class="form-control" type="email" placeholder="ejemplo@correo.com">
-            <br>
-            <label for="telefono" class="form-label">Número de Teléfono</label>
-            <input id="telefono" class="form-control" type="text" placeholder="88886666">
-            <br>
-            <label for="usuario" class="form-label">Nombre de Usuario</label>
-            <input id="usuario" class="form-control" type="text" placeholder="Nombre_Usuario33">
-            <br>
-            <label for="clave" class="form-label">Contraseña</label>
-            <input id="clave" class="form-control" type="password" placeholder="••••••••••••••••">
-            <br>
-            <label for="foto" class="form-label">Foto de Usuario</label>
-            <input class="form-control" type="file" id="foto">
+            <input name="correo" id="correo" class="form-control" type="email" placeholder="ejemplo@correo.com" required>
         </div>
-        <div class="col-md-12 confirmacion">
+        <div class="col-md-12 col-lg-6 mt-3">
+            <label for="telefono" class="form-label">Número de Teléfono</label>
+            <input name="telefono" id="telefono" class="form-control" type="number" placeholder="88886666" required min="0">
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
+            <label for="usuario" class="form-label">Nombre de Usuario</label>
+            <input name="usuario" id="usuario" class="form-control" type="text" placeholder="Nombre_Usuario33" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
+            <label for="clave" class="form-label">Contraseña</label>
+            <input name="clave" id="clave" class="form-control" type="password" placeholder="••••••••••••••••" required>
+        </div>
+        <div class="col-md-12 col-lg-6 mt-3">
+            <label for="foto" class="form-label">Foto de Usuario</label>
+            <input name="foto" id="foto" class="form-control" type="file">
+        </div>
+        <div class="col-md-12 confirmacion mt-4">
             <div class="mb-3 form-check d-block">
-                <label class="form-check-label" for="exampleCheck1"><input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <label class="form-check-label" for="exampleCheck1"><input name="confirmacion" id="confirmacion" type="checkbox" class="form-check-input" required>
                     Al crear una cuenta, acepta nuestra <a data-bs-toggle="modal" data-bs-target="#modal" id="politicas">Política de Privacidad</a></label>
             </div>
             <button type="submit" id="crear-cuenta" class="btn btn-azul">¡Crear Cuenta!</button>
         </div>
-    </div>
+</form>
 
     <!-- Modal -->
     <div class="modal fade" id="modal" tabindex="-1" aria-labelledby="Modal de políticas de Privacidad" aria-hidden="true" data-bs-backdrop="static">
