eb8d07904339753207688ad984c40c5349bfe0fc
Valeria Carvajal <valeriacarvajal1@outlook.com>
2024-04-11 15:58:06 -0600

    Se cargan los datos del usuario en su perfil y validacion de administrador o visitante

--- js/perfil-usuario.js
+++ js/perfil-usuario.js
@@ -1,19 +1,106 @@
+let usuario = sessionStorage.getItem("activo");
+cargarCampos(usuario);
+
 $("#actualizar").click(function (e) {
+    modoEdicion();
+});
+
+$("#cancelar").click(function (e) {
+    deshabilitar();
+    //PENDIENTE VOLVER A LLAMAR LOS DATOS DE LA BASE DE DATOS POR SI HA OCURRIDO ALGUN CAMBIO SIN GUARDAR
+});
+
+$("#confirmar").click(function (e) {
+    guardar();
+});
+
+function modoEdicion() {
     //Abrir campos para editar datos
     $("input").attr("disabled", false);
     $("#cedula").attr("disabled", true);
     $("#cancelar").css("display", "inline");
     $("#confirmar").css("display", "inline");
     $("#actualizar").css("display", "none");
-});
+}
 
-$("#cancelar").click(function (e) {
+function habilitar() {
+    $(".usuario-info input").attr("disabled", false);
+}
+
+function deshabilitar() {
     //reestablecer campos desabilitados y botones
     $("input").attr("disabled", true);
     $("#cancelar").css("display", "none");
     $("#confirmar").css("display", "none");
     $("#actualizar").css("display", "inline");
+}
 
-    //PENDIENTE VOLVER A LLAMAR LOS DATOS DE LA BASE DE DATOS POR SI HA OCURRIDO ALGUN CAMBIO SIN GUARDAR
-});
+function cargarCampos(username) {
+    habilitar();
+    $.ajax({
+        type: "GET",
+        url: `http://localhost:8080/buscar-usuario/${username}`,
+        dataType: "JSON",
+        success: function (response) {
+            //  Obtengo un arreglo de todos los input a llenar por medio de su propiedad name
+            let campos = $('*[name]');
 
+            //Se llenan los campos con los valores obtenidos de la base de datos
+            for (let i = 0; i < campos.length; i++) {
+                if (campos[i].type != "file") {
+                    $(campos[i]).val(response[campos[i].name]);
+                }
+            }
+
+            //Colocar la foto de perfil y nombre de usuario en el encabezado del form
+            $("#foto").attr("src", `img/${response.foto_perfil}`);
+            $("#nombreUsuario").html(response.nombre_usuario)
+
+            //Validacion de tipo de usuario para mostrar opciones
+            if (response.rol_fk == 1) {
+                $("#CRUD-propiedades").attr("hidden", false);
+                $("#reportes").attr("hidden", false);
+                $("#CRUD-usuarios").attr("hidden", false);
+            }
+
+        },
+        error: function (er) {
+            console.log(er.statusText + er.responseText);
+        },
+        complete: function () {
+            console.log("Terminado");
+        }
+    });
+    deshabilitar();
+}
+
+
+function guardar() {
+    //Habilitamos el campo cedula para añadirlo a la variable datos
+    habilitar();
+    const datos = $(".usuario-info").serialize();
+    // console.log(datos);
+    deshabilitar();
+    $.ajax({
+        type: "PUT",
+        url: `http://localhost:8080/modificar-usuario?${datos}`,
+        success: function (response) {
+            Swal.fire({
+                title: "Mensaje",
+                text: "Se han modificado los datos del usuario",
+                icon: "success"
+            });
+
+            //Guardar el nuevo nombre de usuario si se ha cambiado para recargar la página
+            sessionStorage.setItem("activo", $("#nombre_usuario").val());
+            usuario = sessionStorage.getItem("activo");
+            cargarCampos(usuario);
+        },
+        error: function (er) {
+            console.log(er.statusText + er.responseText);
+        },
+        complete: function () {
+            console.log("Terminado");
+        }
+    });
+}
--- perfil-usuario.php
+++ perfil-usuario.php
@@ -1,74 +1,84 @@
 <? include "shared/header.php" ?>
-
-<div class="perfil">
-    <div class="propiedades-usuario">
-        <!-- <label id="cerrar-sesion">Cerrar Sesión</label>
+<main>
+    <div class="perfil">
+        <div class="propiedades-usuario">
+            <!-- <label id="cerrar-sesion">Cerrar Sesión</label>
         <span><button id="" class="btn btn-azul">Mis propiedades</button></span> -->
-        <div class="nav-item dropdown justify-align-content-end">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Opciones
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="CRUD-propiedades.php">Propiedades</a></li>
-            <li><a class="dropdown-item" href="reportes.php">Generar reporte</a></li>
-            <li><a class="dropdown-item" href="CRUD-usuarios.php">Control de usuarios</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
-          </ul>
+            <div class="nav-item dropdown justify-align-content-end">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Opciones
+                </a>
+                <ul class="dropdown-menu">
+                    <li id="CRUD-propiedades" hidden><a class="dropdown-item" href="CRUD-propiedades.php">Propiedades</a></li>
+                    <li id="reportes" hidden><a class="dropdown-item" href="reportes.php">Generar reporte</a></li>
+                    <li id="CRUD-usuarios" hidden><a class="dropdown-item" href="CRUD-usuarios.php">Control de usuarios</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Cerrar Sesión</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-    <!-- <div class="f"> -->
-    
-    <!-- </div> -->
+        <!-- <div class="f"> -->
 
-    <div class="vista">
-        <h1>Mi perfil</h1>
-        <img src="img/Foto-perfil-usuario.jpg" alt="Foto de perfil del usuario">
-        <h3 id="nombre_usuario">Nombre de usuario</h3>
-    </div>
-    <div class="separador"></div>
-    <div class=" row usuario-info">
-        <h2>Información de Cuenta</h2>
-        <br>
-        <div class="col-md-12 col-lg-6">
-            <label for="cedula" class="form-label">Cédula</label>
-            <input name="cedula" id="cedula" class="form-control" type="text" placeholder="112233445" disabled>
-            <br>
-            <label for="nombre" class="form-label">Nombre</label>
-            <input name="nombre" id="nombre" class="form-control" type="text" placeholder="Nombre" disabled>
-            <br>
-            <label for="apellido1" class="form-label">Primer Apellido</label>
-            <input name="apellido1" id="apellido1" class="form-control" type="text" placeholder="Apellido1" disabled>
-            <br>
-            <label for="apellido2" class="form-label">Segundo Apellido</label>
-            <input name="apellido2" id="apellido2" class="form-control" type="text" placeholder="Apellido2" disabled>
-            <br>
-            <label for="fecha_nacimiento" class="form-label">Fecha de Nacimiento</label>
-            <input name="fecha_nacimiento" id="fecha_nacimiento" class="form-control" type="date" disabled>
-            <br>
+        <!-- </div> -->
+
+        <div class="vista">
+            <h1>Mi perfil</h1>
+            <img id="foto" src="img/foto-predeterminada.webp" alt="Foto de perfil del usuario">
+            <h3 id="nombreUsuario">Nombre de usuario</h3>
         </div>
-        <div class="col-md-12 col-lg-6">
-            <label for="correo" class="form-label">Correo Electrónico</label>
-            <input name="correo" id="correo" class="form-control" type="email" placeholder="ejemplo@correo.com" disabled>
+        <div class="separador"></div>
+        <form class="row usuario-info">
+            <h2>Información de Cuenta</h2>
             <br>
-            <label for="telefono" class="form-label">Número de Teléfono</label>
-            <input name="telefono" id="telefono" class="form-control" type="text" placeholder="88886666" disabled>
-            <br>
-            <label for="nombre_usuario" class="form-label">Nombre de Usuario</label>
-            <input name="nombre_usuario" id="nombre_usuario" class="form-control" type="text" placeholder="Nombre_Usuario33" disabled>
-            <br>
-            <label for="clave" class="form-label">Contraseña</label>
-            <input name="clave" id="clave" class="form-control" type="password" placeholder="••••••••••••••••" disabled>
-            <br>
-            <label for="foto" class="form-label">Foto de Usuario</label>
-            <input name="foto" class="form-control" type="file" id="foto" disabled>
-        </div>
-        <div class="col-md-12 col-lg-12 botones">
-            <!-- botones - display: inline -->
-            <button type="button" id="actualizar" class="btn btn-azul">Actualizar datos</button>
-            <button type="button" id="cancelar" class="btn btn-azul">Cancelar</button>
-            <button type="submit" id="confirmar" class="btn btn-success">Confirmar cambios</button>
-        </div>
-    </div>
-</div>
+            <div class="col-md-12 col-lg-6">
+                <label for="cedula" class="form-label">Cédula</label>
+                <input name="cedula" id="cedula" class="form-control" type="text" placeholder="112233445" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="nombre" class="form-label">Nombre</label>
+                <input name="nombre" id="nombre" class="form-control" type="text" placeholder="Nombre" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="apellido1" class="form-label">Primer Apellido</label>
+                <input name="apellido1" id="apellido1" class="form-control" type="text" placeholder="Apellido1" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="apellido2" class="form-label">Segundo Apellido</label>
+                <input name="apellido2" id="apellido2" class="form-control" type="text" placeholder="Apellido2" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="fecha_nacimiento" class="form-label">Fecha de Nacimiento</label>
+                <input name="fecha_nacimiento" id="fecha_nacimiento" class="form-control" type="date" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="correo" class="form-label">Correo Electrónico</label>
+                <input name="correo" id="correo" class="form-control" type="email" placeholder="ejemplo@correo.com" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="telefono" class="form-label">Número de Teléfono</label>
+                <input name="telefono" id="telefono" class="form-control" type="text" placeholder="88886666" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="nombre_usuario" class="form-label">Nombre de Usuario</label>
+                <input name="nombre_usuario" id="nombre_usuario" class="form-control" type="text" placeholder="Nombre_Usuario33" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="clave" class="form-label">Contraseña</label>
+                <input name="clave" id="clave" class="form-control" type="password" placeholder="••••••••••••••••" disabled>
+            </div>
+            <div class="col-md-12 col-lg-6">
+                <label for="foto" class="form-label">Foto de Usuario</label>
+                <input name="foto" class="form-control" type="file" id="foto" disabled>
+            </div>
+            <div class="col-md-12 col-lg-12 botones">
+                <!-- botones - display: inline -->
+                <button type="button" id="actualizar" class="btn btn-azul">Actualizar datos</button>
+                <button type="button" id="cancelar" class="btn btn-azul">Cancelar</button>
+                <button type="button" id="confirmar" class="btn btn-success">Confirmar cambios</button>
+            </div>
+
+        </form>
+</main>
 <? include "shared/footer.php" ?>
